051b77c5f043fe3aeca96634fc3ed5817baef04f
Vlad2358 <107256377+Vlad2358@users.noreply.github.com>
2022-06-10 15:22:21 +0300

    Add files via upload

---
+++ Energy.cs
@@ -0,0 +1,32 @@
+﻿namespace Physics
+{
+    public class Energy
+    {
+        public double v; // скорость может быть меньше нуля
+        private double m;
+
+        public double M // а вот для массы следует добавить валидацию, т. к. её минимальное значение - 0
+        {
+            get { return m; }
+            set 
+            {
+                if(value >= 0)
+                { 
+                    m = value;
+                }
+            }
+        }
+
+        public Energy(double v, double m)
+        {
+            this.v = v;
+            M = m;
+        }
+
+        public double CalcEnergy()
+        {
+            var e = (m * Math.Pow(v, 2)) / 2;
+            return e;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Physics;
+
+var isWorking = true;
+while (isWorking)
+{
+    Console.WriteLine("1. Вычислить энергию 2. Выйти");
+    var value = Console.ReadLine();
+    switch (value)
+    {
+        case "1":
+            Counting();
+            break;
+        case "2":
+            isWorking = false;
+            break;
+        default:
+            break;
+    }
+}
+
+void Counting()
+{
+    try
+    {
+        Console.Write("Введите скорость: ");
+        var v = double.Parse(Console.ReadLine());
+        Console.Write("Введите массу: ");
+        var m = double.Parse(Console.ReadLine());
+        var e = new Energy(v, m);
+        Console.WriteLine("Энергия тела = " + e.CalcEnergy().ToString());
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+}
